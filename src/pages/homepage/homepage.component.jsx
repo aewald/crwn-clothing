db9c15bf07d920eba556d4538620774f53b716cb
Andy Ewald <aewald@Andys-Mac-mini.local>
2020-09-22 18:35:24 -0300

    file structure into components and styling menu

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './hompage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => <Directory />;
+
+export default HomePage;
