141f8a5a3ec1d85a5affc959fb21472d0f969faf
Andy Ewald <aewald@Andys-Mac-mini.local>
2020-09-24 09:28:55 -0300

    sign in and form components

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import FormInput from '../form-input/form-input.components';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+		};
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		this.setState({ email: '', password: '' });
+	};
+
+	handleChange = (event) => {
+		const { value, name } = event.target;
+
+		this.setState({ [name]: value });
+	};
+
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I already have an account</h2>
+				<span>Sign in with your email and password</span>
+
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						label="Email"
+						name="email"
+						type="email"
+						value={this.state.email}
+						onChange={this.handleChange}
+						required
+					/>
+					<FormInput
+						label="Password"
+						name="password"
+						type="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+						required
+					/>
+
+					<CustomButton type="submit">SIGN IN</CustomButton>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SignIn;
